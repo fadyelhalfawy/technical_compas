f8d56e457aa97440779addb096803272121b59ce
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-10-23 06:13:50 +0300

    Create FooterJS file.

---
+++ compas/src/Reducer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className='background-img'>
+      </div>
+
+      <div className='footer-text'>
+        <p>
+            © ITWorx All Rights Reserved
+        </p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Footer;
