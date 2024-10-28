c6f9a4d5b953f26d7ffb32a9843b1d87bde0a8c3
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-10-28 10:25:31 +0300

    Competencies JS
    Competencies page to load the table of competencies.

---
+++ compas/src/Pages/Competencies.js
@@ -0,0 +1,61 @@
+import { useLocation } from 'react-router-dom';
+import jsonData from '../JSON/Competencies.json';
+import Navbar from '../Reducer/NavBar';
+import Footer from '../Reducer/Footer';
+
+const Competencies = () => {
+    const location = useLocation();
+    const { competencie, level } = location.state || {};
+    
+    const getCompetencie = jsonData.competencies;
+    const selectedCompetencie = getCompetencie.find(comp => comp.name === competencie.toLowerCase());
+
+    const getLevel = selectedCompetencie ? selectedCompetencie.levels : [];
+    const selectedLevel = getLevel.find(lev => lev.level === level);
+    
+    return (
+        <>
+        <Navbar />
+        <main>
+            <div className="max-w-full overflow-x-auto p-24">
+                <h1 className='text-center text-4xl font-semibold mb-4'>Competencies</h1>
+          <h2 className="text-2xl font-bold mb-4">{selectedLevel.competency} - {level}</h2>
+          <table className="min-w-full border-collapse border border-gray-300 text-left">
+            <thead>
+                <tr>
+                <th className="border border-gray-300 px-4 py-2 font-semibold text-md text-center">Competency</th>
+                <th className="border border-gray-300 px-5 py-2 font-semibold text-md text-center">Levels</th>
+                <th className="border border-gray-300 px-4 py-2 font-semibold text-md text-center">Definition</th>
+                <th className="border border-gray-300 px-4 py-2 font-semibold text-md text-center">Overview</th>
+                <th className="border border-gray-300 px-4 py-2 font-semibold text-md text-center">Behavioral Indicators</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td className="border border-gray-300 p-1">{selectedLevel.competency}</td>
+                <td className="border border-gray-300 px-4 py-2">{level}</td>
+                <td className="border border-gray-300 px-4 py-2">{selectedLevel.definition}</td>
+                <td className="border border-gray-300 px-4 py-2">{selectedLevel.overview}</td>
+                <td className="border border-gray-300 px-4 py-2">
+                    <ul className='list-disc list-inside space-y-2 m-2'>
+                        {selectedLevel.behavioral.map((item, index) => (
+                        <li key={index}>
+                            {item}
+                        </li>
+          ))}
+        </ul>
+
+                </td>
+                </tr>
+                
+            </tbody>
+            </table>
+        </div>
+        </main>
+        <Footer />
+        </>
+        
+    );
+  }
+  
+  export default Competencies;
