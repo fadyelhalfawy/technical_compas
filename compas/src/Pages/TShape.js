ce1991289c66e21e30fd1729170838bc4948bbde
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-11-01 20:56:59 +0200

    Update Tshape page
    By adding the new elemtens to display the data from the database.

--- compas/src/Pages/TShape.js
+++ compas/src/Pages/TShape.js
@@ -1,6 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Navbar from '../Reducer/NavBar';
+import Footer from '../Reducer/Footer';
+
 const TShape = () => {
+    const location = useLocation();
+    const { selectedTable, selectedTitle } = location.state || {};
+    const [data, setData] = useState("");
+    console.log(selectedTable, selectedTitle);
+    
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/nodejs`).then(response => response.json())
+        .then(data => setData(data[0]))
+        .catch(error => console.error('Error fetching data:', error));
+      }, [])
+
+    
+    const entries = Object.entries(data);
+
     return(
-        <h1>Hello World</h1>
+        <>
+        <Navbar />
+        
+        <main>
+            <div className="overflow-x-auto p-24">
+            <h1 className='text-center text-4xl font-semibold mb-4'>T-Shape Skills</h1>
+            <h2 className="text-2xl font-bold mb-4 text-gray-600">{selectedTable} - {selectedTitle}</h2>
+        <table className="min-w-full border-collapse border border-gray-200">
+  <thead>
+    <tr className="bg-gray-100">
+      <th className="px-4 md:px-6 py-3 border border-gray-300 text-gray-700 font-semibold text-center text-sm md:text-base">Technology</th>
+      <th className="px-4 md:px-6 py-3 border border-gray-300 text-gray-700 font-semibold text-center text-sm md:text-base">Experience Level</th>
+      <th className="px-4 md:px-6 py-3 border border-gray-300 text-gray-700 font-semibold text-center text-sm md:text-base">Notes</th>
+    </tr>
+  </thead>
+  <tbody>
+  {entries.map(([row, value], index) => (
+          <tr className="odd:bg-white even:bg-gray-50" key={index}>
+            <td className="px-4 md:px-6 py-2 md:py-4 border border-gray-300 text-gray-700 text-center text-sm md:text-base">{row}</td>
+            <td className="px-4 md:px-6 py-2 md:py-4 border border-gray-300 text-gray-700 text-center text-sm md:text-base">{value ?? "N/A"}</td>
+          </tr>
+        ))}
+  </tbody>
+</table>
+        </div>
+        </main>
+        <Footer />
+        </>
     )
 }
 
