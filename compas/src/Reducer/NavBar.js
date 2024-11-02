e80b71a5c64fba47cb4202df12db7f6fa4e844d0
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-11-02 18:33:29 +0200

    Update navbar
    by resizing the itworx image.

--- compas/src/Reducer/NavBar.js
+++ compas/src/Reducer/NavBar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   return (
     <nav className="text-white p-1 md:p-3 bg-[#6b1d1b]">
-        <img className='w-4/12 h-4/12 md:w-3/12 md:h-3/12' src={itworx} alt='itworx-image' />
+        <img className='w-4/12 h-4/12 md:w-2/12 md:h-2/12' src={itworx} alt='itworx-image' />
         <h3 className='text-center text-xl md:text-2xl'>{menuItems}</h3>
     </nav>
   );
