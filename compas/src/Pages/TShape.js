75192f2a40be721e1bbc01d416681c97342313af
Fadyhalfawy <fadyhalfawy@gmail.com>
2024-11-11 19:25:52 +0200

    Update TShape file by adding useeffect to render once.

--- compas/src/Pages/TShape.js
+++ compas/src/Pages/TShape.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Navbar from '../Reducer/NavBar';
-import Footer from '../Reducer/Footer';
+import Navbar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const TShape = () => {
     const location = useLocation();
     const { selectedTable, selectedTitle } = location.state || {};
-    const [data, setData] = useState("");
+    const [expValue, setData] = useState([]);
+    const [notesValue, setNotes] = useState([]);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/tshape?table=${selectedTable.toLowerCase()}&title=${selectedTitle.toString()}`)
-        .then(response => response.json())
-        .then(data => setData(data[0]))
-        .catch(error => console.error('Error fetching data:', error));
-      }, [])
-
+    const keyTable = selectedTable.includes(" ") || selectedTable.includes("-") ? selectedTable.replaceAll(" ", "_").replaceAll("-", "_").toLowerCase() : selectedTable.toLowerCase();
+    console.log(selectedTable, selectedTitle, keyTable);
     
-    const entries = Object.entries(data);
+    useEffect(() => {
+        fetch(`http://localhost:5000/tshape?table=${keyTable}&title=${selectedTitle}`)
+        .then(response => response.json())
+        .then(data => {
+           setData(data.expValue[0]);
+           setNotes(data.notesValue[0]);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+      }, [keyTable, selectedTitle])
 
+      const expValueEntries = Object.entries(expValue).slice(1); 
+      const notesValueEntries = Object.values(notesValue).slice(1);
+      console.log(expValueEntries);
+      console.log(notesValueEntries);
+      
+      
+    
     return(
         <>
         <Navbar />
@@ -35,12 +46,16 @@ const TShape = () => {
     </tr>
   </thead>
   <tbody>
-  {entries.slice(1).map(([row, value], index) => (
-          <tr className="odd:bg-white even:bg-gray-50" key={index}>
-            <td className="px-4 md:px-6 py-2 md:py-4 border border-gray-300 text-gray-700 text-center text-sm md:text-base">{row}</td>
-            <td className="px-4 md:px-6 py-2 md:py-4 border border-gray-300 text-gray-700 text-center text-sm md:text-base">{value ?? "N/A"}</td>
-          </tr>
-        ))}
+  {expValueEntries.map(([row, expValue], index) => {
+                const notes = notesValueEntries[index];
+                return (
+                  <tr className="odd:bg-white even:bg-gray-50" key={index}>
+                    <td className="px-4 md:px-6 py-2 md:py-4 border border-gray-300 text-gray-700 text-center text-sm md:text-base">{row.replaceAll("_", " ")}</td>
+                    <td className="px-4 md:px-6 py-2 md:py-4 border border-gray-300 text-gray-700 text-center text-sm md:text-base">{expValue ?? "N/A"}</td>
+                    <td className="px-4 md:px-6 py-2 md:py-4 border border-gray-300 text-gray-700 text-center text-sm md:text-base">{notes ?? "N/A"}</td>
+                  </tr>
+                );
+              })}
   </tbody>
 </table>
         </div>
